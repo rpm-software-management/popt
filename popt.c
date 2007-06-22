f2754c7394bba39e9999c106148aa3dcb3110db5
Jeff Johnson <n3npq@mac.com>
2007-06-22 00:02:01 +0000

    - fix index thinko.

--- popt.c
+++ popt.c
@@ -396,7 +396,7 @@ static int execCommand(poptContext con)
     if (!strchr(item->argv[0], '/') && con->execPath != NULL) {
 	char *s = malloc(strlen(con->execPath) + strlen(item->argv[0]) + sizeof("/"));
 	if (s)
-	    sprintf(s, "%s/%s", con->execPath, item->argv[-1]);
+	    sprintf(s, "%s/%s", con->execPath, item->argv[0]);
 	argv[argc] = s;
     } else
 	argv[argc] = POPT_findProgramPath(item->argv[0]);
