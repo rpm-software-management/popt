7219e1ddc1e8606dda18c1105df0d45d8e8e0e09
Richard Levitte <richard@levitte.org>
2020-06-29 11:56:00 -0400

    Fix incorrect handling of leftovers with poptStuffArgs
    If poptStuffArgs() is used twice with the same context, it will invariably
    cause memory corruption and possibly memory leaks or a crash.
    
    Change the allocation of leftOvers so it adapts to the input on the fly
    instead of trying to pre-allocate it in one go.

--- src/poptint.h
+++ src/poptint.h
@@ -94,6 +94,7 @@ struct poptContext_s {
     struct optionStackEntry * os;
     poptArgv leftovers;
     int numLeftovers;
+    int allocLeftovers;
     int nextLeftover;
     const struct poptOption * options;
     int restLeftover;
